459ba871257edc1ddb22c912ba0773db9e888b98
jatin34090 <jatin34090@gmail.com>
2024-06-25 22:12:26 +0530

    Solve a small error

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { body, validationResult } = require('express-validator');
